67bd8032f3fb77301c1d89cf274a23dafced2b76
Dragisa Stankovski <dragisa1984@yahoo.com>
2020-03-25 09:46:39 +0100

    Delete selected row in table and from database

--- jsf-jpa-simple-app/src/main/java/com/eng/it/controller/SubjectController.java
+++ jsf-jpa-simple-app/src/main/java/com/eng/it/controller/SubjectController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -22,6 +24,8 @@ public class SubjectController {
 
 	private List<Subject> subjectsList;
 
+	private Subject selectedSubject = null;
+
 	@PostConstruct
 	public void init() {
 		subjectDao = new SubjectDao();
@@ -33,12 +37,38 @@ public class SubjectController {
 		return "addSubject";
 	}
 
+	public String updateSubject() {
+		return "addSubject";
+	}
+
+	public void deleteSubject() {
+
+		if (selectedSubject != null) {
+			subjectDao.delete(selectedSubject);
+			subjectsList.remove(selectedSubject);
+
+			selectedSubject = null;
+		} else {
+			FacesMessage msg = new FacesMessage("You need to select subject in table, which you wanna delete...");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+
+	}
+
 	public List<Subject> getSubjectsList() {
 		return subjectsList;
 	}
 
 	public void setSubjectsList(List<Subject> subjectsList) {
 		this.subjectsList = subjectsList;
+	}
+
+	public Subject getSelectedSubject() {
+		return selectedSubject;
+	}
+
+	public void setSelectedSubject(Subject selectedSubject) {
+		this.selectedSubject = selectedSubject;
 	}
 
 }
